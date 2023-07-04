737ee977d8560d799a80e71a3eab197faf5eb624
Alan Myers <138080754+uvaarm91@users.noreply.github.com>
2023-07-04 12:02:41 -0400

    Create SayHello.java

---
+++ SayHello.java
@@ -0,0 +1,9 @@
+public class SayHello
+{
+   public static void main(String[] args)
+   {
+       System.out.print("Hi ");
+       System.out.print("there");
+       System.out.println("!");
+   }
+}
